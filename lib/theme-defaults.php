dfcc26cd04bc462340e782efcdb74a1df19e4621
Wayne  Bishop <waynebbishop@gmail.com>
2016-12-15 22:21:44 +0000

    Update theme-defaults.php

--- lib/theme-defaults.php
+++ lib/theme-defaults.php
@@ -18,10 +18,12 @@ add_filter( 'genesis_theme_settings_defaults', 'genesis_sample_theme_defaults' )
 */
 function genesis_sample_theme_defaults( $defaults ) {
 
-	$defaults['blog_cat_num']              = 6;
-	$defaults['content_archive']           = 'full';
+	$defaults['blog_cat_num']              = 100; /* S1 */
+	$defaults['content_archive']           = 'excerpts'; /* S1 */
 	$defaults['content_archive_limit']     = 0;
-	$defaults['content_archive_thumbnail'] = 0;
+	$defaults['content_archive_thumbnail'] = 1; /* S1 */
+	$defaults['image_size']                = 'thumbnail'; /* S1 */
+	$defaults['image_alignment']           = 'alignright'; /* S1 */
 	$defaults['posts_nav']                 = 'numeric';
 	$defaults['site_layout']               = 'content-sidebar';
 
@@ -40,16 +42,18 @@ function genesis_sample_theme_setting_defaults() {
 	if ( function_exists( 'genesis_update_settings' ) ) {
 
 		genesis_update_settings( array(
-			'blog_cat_num'              => 6,	
-			'content_archive'           => 'full',
+			'blog_cat_num'              => 100, /* S1 */
+			'content_archive'           => 'excerpts', /* S1 */
 			'content_archive_limit'     => 0,
-			'content_archive_thumbnail' => 0,
+			'content_archive_thumbnail' => 1, /* S1 */
+			'image_size'                => 'thumbnail', /* S1 */
+			'image_alignment'           => 'alignright', /* S1 */
 			'posts_nav'                 => 'numeric',
 			'site_layout'               => 'content-sidebar',
 		) );
 		
 	} 
 
-	update_option( 'posts_per_page', 6 );
+	update_option( 'posts_per_page', 100 ); /* S1 */
 
 }
